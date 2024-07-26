c0c9be31be06c480321c1169e2b485d49fb8e8aa
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-07-27 00:07:39 +0300

    Salom

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -41,17 +41,15 @@ const Main = () => {
     }
     const Data4 = {
         title:"Кресло INDI CA-1613A-1",
-        text:"5%",
         price:"5 ₽",
         img:Rasm_4,
-        img1:Rasm_7,
         button:"Add To Cart "
     }
     const Data5 = {
         img:Rasm_5,
         img1:Rasm_7,
         title:"Galaxy Tab S6",
-        text:"10%",
+        text:"5%",
         price:"25 ₽",
         button:"Add To Cart "
     }
@@ -85,8 +83,8 @@ const Main = () => {
                             </div>
                         </div>
                     <div className='card'>
-                    <div className='Cards2'>
-                        <div className='container Cards2-container'>
+                        <div className='Cards2'>
+                            <div className='container Cards2-container'>
                                 <h1 className='Cards2-title'>{Data2.title}</h1>
                                 <img className='Cards2-img-a' src={Data2.img1} alt="" />
                                 <p className='Cards2-text'>{Data2.text}</p>
@@ -94,8 +92,8 @@ const Main = () => {
                                 <br/>
                                 <button className='Cards2-btn'>{Data2.button}</button>
                                 <img className='Cards2-img' src={Data2.img}  alt="" />
+                            </div>
                         </div>
-                    </div>
                    <div className='Cards3'>
                         <div className='container Cards3-container'>
                                <img className='Cards3-img' src={Data3.img}  alt="" />
@@ -104,45 +102,45 @@ const Main = () => {
                                <p className='Cards3-text'>{Data3.text}</p>
                                <span className='Cards3-span'>{Data3.price}</span>
                                <button className='Cards3-btn'>{Data3.button}</button>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
-            <div className='hunner'>
-            <div className='card'>
-                <div className='Cards4'>
-                    <div className='container Cards4-container'>
-                        <h1 className='Cards4-title'>{Data4.title}</h1>
-                        <img className='Cards4-img-a' src={Data4.img1} alt="" />
-                        <p className='Cards4-text'>{Data4.text}</p>
-                        <span className='Cards4-span'>{Data4.price}</span>
-                        <img className='Cards4-img' src={Data4.img}  alt="" />
-                       <button className='Cards4-btn'>{Data4.button}</button>
+                <div className='hunner'>
+                    <div className='card'>
+                        <div className='Cards4'>
+                            <div className='container Cards4-container'>
+                                <h1 className='Cards4-title'>{Data4.title}</h1>
+                                <img className='Cards4-img-a' src={Data4.img1} alt="" />
+                                <p className='Cards4-text'>{Data4.text}</p>
+                                <span className='Cards4-span'>{Data4.price}</span>
+                                <img className='Cards4-img' src={Data4.img}  alt="" />
+                                <button className='Cards4-btn'>{Data4.button}</button>
+                            </div>
+                        </div>
+                    <div className='Cards5'>
+                <div className='container Cards5-container'>
+                                <img className='Cards5-img' src={Data5.img}  alt="" />
+                                <h1 className='Cards5-title'>{Data5.title}</h1>
+                                <img className='Cards5-img-a' src={Data5.img1} alt="" />
+                                <p className='Cards5-text'>{Data5.text}</p>
+                                <span className='Cards5-span'>{Data5.price}</span>
+                                <button className='Cards5-btn'>{Data5.button}</button>
+                            </div>
+                        </div>
+                    <div className='Cards6'>
+                <div className='container Cards6-container'>
+                                <img className='Cards-img' src={Data6.img}  alt="" />
+                                <h1 className='Cards-title'>{Data6.title}</h1>
+                                <img className='Cards-img-a' src={Data6.img1} alt="" />
+                                <p className='Cards-text'>{Data6.text}</p>
+                                <span className='Cards-span'>{Data6.price}</span>
+                                <button className='Cards-btn'>{Data6.button}</button>
+                            </div>
+                        </div>
                     </div>
-               </div>
-               <div className='Cards5'>
-                    <div className='container Cards5-container'>
-                        <img className='Cards-img' src={Data5.img}  alt="" />
-                        <h1 className='Cards-title'>{Data5.title}</h1>
-                        <img className='Cards-img-a' src={Data5.img1} alt="" />
-                        <p className='Cards-text'>{Data5.text}</p>
-                        <span className='Cards-span'>{Data5.price}</span>
-                       <button className='Cards-btn'>{Data5.button}</button>
-                    </div>
-               </div>
-               <div className='Cards6'>
-                    <div className='container Cards6-container'>
-                        <img className='Cards-img' src={Data6.img}  alt="" />
-                        <h1 className='Cards-title'>{Data6.title}</h1>
-                        <img className='Cards-img-a' src={Data6.img1} alt="" />
-                        <p className='Cards-text'>{Data6.text}</p>
-                        <span className='Cards-span'>{Data6.price}</span>
-                       <button className='Cards-btn'>{Data6.button}</button>
-                    </div>
-               </div>
-            </div>
-            </div>
+                </div>
             </div>
         </div>
     </div>
