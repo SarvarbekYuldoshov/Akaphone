65b1431968e1f0ea7f72ffbae1f268268360ccbf
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-18 16:30:14 +0300

    salolar

--- src/Components/Store/Store.jsx
+++ src/Components/Store/Store.jsx
@@ -1,8 +1,6 @@
  import { create } from "zustand";
 
  const useSharedStore = create ((set)=>({
-   // count: 0,
-   // setCount: (newCount)=> set({count:newCount})
    cars:[] ,
    setCars: (newCars)=> set({cars:newCars})
  }))
